c8462fb9d77e8dc36e9683184853754cf4984b2c
Jawad <Jawadsadiq7373@gmail.com>
2025-05-01 09:08:07 -0700

    Counter app finished

--- Counter_App/src/App.jsx
+++ Counter_App/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Counter from "./components/Counter";
 
 export default function App() {
--- Counter_App/src/components/Counter.jsx
+++ Counter_App/src/components/Counter.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "../style.css";
+import { useState } from "react";
 
 export default function Counter() {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div className="container">
+      <div>
+        <h1 className="number">{count}</h1>
+
+        <div className="btns-container">
+          <button onClick={() => setCount(count + 1)} className="btn">
+            +
+          </button>
+          <button onClick={() => setCount(0)} className="btn-reset btn">
+            Reset
+          </button>
+          <button onClick={() => setCount(count - 1)} className="btn">
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
